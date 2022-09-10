3d16069320abb57540682c27967d83c95a8891c8
tzsoulcap <tzsoulcap@gmail.com>
2022-09-10 22:54:43 +0700

    Update 1_Describe_Dataset_from_URL.py

--- pages/1_Describe_Dataset_from_URL.py
+++ pages/1_Describe_Dataset_from_URL.py
@@ -14,6 +14,16 @@ st.dataframe(data)
 option_gb = data.columns.values
 option_gb = pd.DataFrame(option_gb).append({0:'-'}, ignore_index=True).sort_values([0])
 
+
 st.sidebar.selectbox('Group by', option_gb, key='groupby')
 st.sidebar.write(st.session_state.groupby)
-st.dataframe(option_gb)
+
+st.sidebar.multiselect('Select data for bar chart', option_gb, key='mtselect')
+st.sidebar.write(st.session_state.mtselect)
+selected = st.session_state.mtselect
+prepare = data[selected].sum().to_frame()
+# prepare.rename({0:'Video Games'}, axis='columns', inplace=True)
+
+st.sidebar.dataframe(prepare)
+# barchart = data.loc[:, ['Action', 'Adventure']]
+st.bar_chart(prepare)
