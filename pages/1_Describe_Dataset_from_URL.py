459e7e1b3bafa681172dac7f664275fea5daaf46
tzsoulcap <tzsoulcap@gmail.com>
2022-09-10 22:58:25 +0700

    Update 1_Describe_Dataset_from_URL.py

--- pages/1_Describe_Dataset_from_URL.py
+++ pages/1_Describe_Dataset_from_URL.py
@@ -8,22 +8,23 @@ st.sidebar.header('input')
 st.sidebar.text_input('URL', key='url')
 st.warning(f"Dataset from {st.session_state.url}" if st.session_state.url != '' else 'Please enter dataset url')
 
-data = pd.read_csv(st.session_state.url)
-st.dataframe(data)
+if st.session_state.url:
+    data = pd.read_csv(st.session_state.url)
+    st.dataframe(data)
 
-option_gb = data.columns.values
-option_gb = pd.DataFrame(option_gb).append({0:'-'}, ignore_index=True).sort_values([0])
+    option_gb = data.columns.values
+    option_gb = pd.DataFrame(option_gb).append({0:'-'}, ignore_index=True).sort_values([0])
 
 
-st.sidebar.selectbox('Group by', option_gb, key='groupby')
-st.sidebar.write(st.session_state.groupby)
+    st.sidebar.selectbox('Group by', option_gb, key='groupby')
+    st.sidebar.write(st.session_state.groupby)
 
-st.sidebar.multiselect('Select data for bar chart', option_gb, key='mtselect')
-st.sidebar.write(st.session_state.mtselect)
-selected = st.session_state.mtselect
-prepare = data[selected].sum().to_frame()
-# prepare.rename({0:'Video Games'}, axis='columns', inplace=True)
+    st.sidebar.multiselect('Select data for bar chart', option_gb, key='mtselect')
+    st.sidebar.write(st.session_state.mtselect)
+    selected = st.session_state.mtselect
+    prepare = data[selected].sum().to_frame()
+    # prepare.rename({0:'Video Games'}, axis='columns', inplace=True)
 
-st.sidebar.dataframe(prepare)
-# barchart = data.loc[:, ['Action', 'Adventure']]
-st.bar_chart(prepare)
+    st.sidebar.dataframe(prepare)
+    # barchart = data.loc[:, ['Action', 'Adventure']]
+    st.bar_chart(prepare)
